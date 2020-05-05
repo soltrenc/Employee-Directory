f7a56acb8123ee98748b628181cf9d0c5410a350
Chris Soltren <csoltren1@yahoo.com>
2020-05-05 19:20:58 -0400

    Added more results

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export default {
   getRandomEmployees: function () {
-    return axios.get('https://randomuser.me/api/?results=10');
+    return axios.get('https://randomuser.me/api/?results=20');
   }
 };
